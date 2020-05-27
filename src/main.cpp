fb39df4a69a84bbbbf8915380462ed8291e3375f
ÆJT <aejontargaryen@protonmail.com>
2020-05-27 02:06:21 -0400

    copyright 2020

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2011-2015 The Cryptonote developers
 // Copyright (c) 2016-2017 The Karbowanec developers
-// Copyright (c) 2017-2019 The Fandom Gold Project
+// Copyright (c) 2017-2020 The Fandom Gold Project
 
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
